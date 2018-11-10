dd812e44afab90292b1d3a3f8114dd225feaca57
ne_tru <fadeout01@gmail.com>
2018-11-10 12:34:57 +0300

    showing the request payload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -26,18 +27,30 @@ var server = http.createServer(function(req, res) {
     // Get the headers as an object
     var headers = req.headers;
 
-    // Send the response
-    res.end('Hello World\n');
+    // Get the payload, if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
 
-    // Log the request path
-    console.log('Request received on path: '
-        + trimmedPath
-        + ' with this method: '
-        + method
-        + ' and with these query string parameters ', 
-        queryStringObject);
+        // Send the response
+        res.end('Hello World\n');
 
-    console.log('Request received with these headers: ', headers);
+        // Log the request path
+        console.log('Request received on path: '
+            + trimmedPath
+            + ' with this method: '
+            + method
+            + ' and with these query string parameters ', 
+            queryStringObject);
+
+        console.log('Request received with these headers: ', headers);
+        console.log('Request received with this payload: ', buffer);
+    });
+
 });
  
 // Start the server, and have it listen on port 3000

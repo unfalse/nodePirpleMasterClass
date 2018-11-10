9191778f494a043b3505531b1f72ee11734d3807
ne_tru <fadeout01@gmail.com>
2018-11-10 11:15:38 +0300

    playing with paths

--- index.js
+++ index.js
@@ -5,10 +5,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
+    
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request received on path: ' + trimmedPath);
+
 });
  
 // Start the server, and have it listen on port 3000
